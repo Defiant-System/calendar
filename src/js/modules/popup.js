6ba2a671436aebe74862999db149026739ef7c60
Hakan Bilgin <hbi99@hotmail.com>
2021-02-24 00:34:38 +0100

    popup can show outside app borders

--- src/js/modules/popup.js
+++ src/js/modules/popup.js
@@ -12,19 +12,28 @@
 		let APP = calendar,
 			Self = APP.popup,
 			append,
+			match,
+			rect,
+			top,
+			left,
+			popup,
+			pEl,
 			el;
 		switch (event.type) {
 			case "popup-event-details":
 				// DOM element to append popup
-				append = event.el.parents(".days-wrapper");
+				append = event.el.parents(".days");
 				// remove potential existing popup
 				append.find(".popup-event").remove();
+				// xpath matching event node
+				match = `//event[@id="${event.el.data("id")}"]`;
 				// render event details
-				window.render({
-					template: "popup-event",
-					match: `//event[@id="${event.el.data("id")}"]`,
-					append,
-				});
+				popup = window.render({ template: "popup-event", match, append });
+
+				pEl = event.el.parent();
+				top = (event.el.prop("offsetTop") - append.find(".days-wrapper").prop("scrollTop")) +"px";
+				left = (pEl.prop("offsetLeft") + pEl.prop("offsetWidth") + 11) +"px";
+				popup.css({ top, left });
 				break;
 		}
 	}
