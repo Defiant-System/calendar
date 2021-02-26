a78ae2ac24ed56c07923c5a37c37104d0c58cd3f
Hakan Bilgin <hbi99@hotmail.com>
2021-02-26 16:55:47 +0100

    changing event color via contextmenu

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,15 @@
 @import "./modules/render.js";
 @import "./modules/view.js";
 
+const Palette = {
+		"#64b2eb": "blue",
+		"#f67c7c": "red",
+		"#d38df4": "purple",
+		"#f29d5c": "orange",
+		"#faea4b": "yellow",
+		"#96f67d": "green",
+		"#9fe5e5": "cyan",
+	};
 
 const calendar = {
 	init() {
@@ -65,6 +74,8 @@ const calendar = {
 				break;
 			// contextmenu events
 			case "change-event-calendar":
+				event.origin.el.prop({ className: `event ${Palette[event.arg]}` });
+				break;
 			case "event-calendar-info":
 			case "delete-event-calendar":
 				console.log(event);
